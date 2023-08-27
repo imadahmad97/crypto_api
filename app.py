69416227e0e73cb11b00dc185fd45864963a5464
Imad Ahmad <imadahmad97@yahoo.ca>
2023-08-27 02:55:50 -0700

    changed port and redirect

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ from fetch_price import get_crypto_prices
 
 app = Flask(__name__)
 
-@app.route("/crypto-prices", methods=["GET"])
+@app.route("/", methods=["GET"])
 def crypto_prices():
     prices = get_crypto_prices()
     return jsonify(prices)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(port=5000)
