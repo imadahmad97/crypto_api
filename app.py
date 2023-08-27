6479b4cff3609d21aa57d497f13af4c1d08012f3
Imad Ahmad <imadahmad97@yahoo.ca>
2023-08-27 01:52:58 -0700

    reorganized app and func

--- app.py
+++ app.py
@@ -1,37 +1,12 @@
 from flask import Flask, jsonify
-import requests
-from bs4 import BeautifulSoup as bs
+from fetch_price import get_crypto_prices
 
 app = Flask(__name__)
 
 @app.route("/crypto-prices", methods=["GET"])
-def get_crypto_prices():
-    btc_url = 'https://www.coindesk.com/price/bitcoin/'
-    eth_url = 'https://www.coindesk.com/price/ethereum/'
-    doge_url = 'https://www.coindesk.com/price/dogecoin/'
-    ltc_url = 'https://www.coindesk.com/price/litecoin/'
-
-    btc_req = requests.get(btc_url)
-    eth_req = requests.get(eth_url)
-    doge_req = requests.get(doge_url)
-    ltc_req = requests.get(ltc_url)
-
-    btc_soup = bs(btc_req.content, "html.parser")
-    eth_soup = bs(eth_req.content, "html.parser")
-    doge_soup = bs(doge_req.content, "html.parser")
-    ltc_soup = bs(ltc_req.content, "html.parser")
-
-    btc_price = btc_soup.find("span", {"class": "currency-pricestyles__Price-sc-1rux8hj-0 eEpEzP"}).text
-    eth_price = eth_soup.find("span", {"class": "currency-pricestyles__Price-sc-1rux8hj-0 eEpEzP"}).text
-    doge_price = doge_soup.find("span", {"class": "currency-pricestyles__Price-sc-1rux8hj-0 jxzQXk"}).text
-    ltc_price = ltc_soup.find("span", {"class": "currency-pricestyles__Price-sc-1rux8hj-0 eEpEzP"}).text
-
-    return jsonify({
-        "BTC": btc_price,
-        "ETH": eth_price,
-        "DOGE": doge_price,
-        "LTC": ltc_price
-    })
+def crypto_prices():
+    prices = get_crypto_prices()
+    return jsonify(prices)
 
 if __name__ == "__main__":
     app.run(debug=True)
